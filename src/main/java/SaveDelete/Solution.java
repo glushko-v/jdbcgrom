264172f5c003f15e71cabe1bf973803ab5a3cf06
glushko-v <glushko@mail.com>
2019-04-24 15:09:40 +0300

    jdbc SaveDelete

--- src/main/java/SaveDelete/Solution.java
+++ src/main/java/SaveDelete/Solution.java
@@ -11,51 +11,27 @@ public class Solution {
     public static final String PASS = "test123456";
 
 
-    public Product saveProduct() {
+    public void saveProduct() {
 
         try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);
              Statement statement = connection.createStatement()) {
-//
-//
-//            int response = statement.executeUpdate("INSERT INTO PRODUCT VALUES (999, 'toy', 'for children', 60)");
-//
-//            System.out.println(response);
-//
-//
-//        } catch (SQLException se) {
-//            System.err.println("Something went wrong");
-//            se.printStackTrace();
-//
-//
-//        }
 
-        try (ResultSet rs = statement.executeQuery("INSERT INTO PRODUCT VALUES (999, 'toy', 'for children', 60)")) {
-            while (rs.next()) {
-                long id = rs.getLong(1);
-                String name = rs.getString(2);
-                String description = rs.getString(3);
-                int price = rs.getInt(4);
 
-                Product product = new Product(id, name, description, price);
+            int response = statement.executeUpdate("INSERT INTO PRODUCT VALUES (999, 'toy', 'for children', 60)");
 
-                return product;
+            System.out.println(response);
 
-            }
+
+        } catch (SQLException se) {
+            System.err.println("Something went wrong");
+            se.printStackTrace();
+
 
         }
 
 
-    } catch (SQLException se) {
-        System.err.println("Something went wrong");
-        se.printStackTrace();
-
-
     }
 
-
-    return null;
-
-    }
 
     public void deleteProducts() {
 
